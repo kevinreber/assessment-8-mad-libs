de481bc932a2e21bbad1ef76454201743dae08f8
kevinreber <kevinreber1@gmail.com>
2020-07-04 10:48:00 -0700

    part 3: styled form and added form toggle

--- Part 3-React MadLibs/src/App.js
+++ Part 3-React MadLibs/src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [showForm, setShowForm] = useState(true);
+
+  // useEffect(() =>{
+    
+
+  // },[])
+
+  const toggleForm = (e) =>{
+    e.preventDefault();
+    setShowForm(f => !f);    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>Madlibs!</h1>
+    </header>
+    <form>
+      { showForm ? 
+      ( 
+        <>   
+        <input name="noun-1" placeholder="noun"/>
+        <input name="noun-2" placeholder="noun 2"/>
+        <input name="adjective" placeholder="adjective"/>
+        <input name="color" placeholder="color"/>
+        </>
+      )
+        :
+        (<h2>hello</h2>)
+      }
+
+      <button 
+        onClick={toggleForm}>
+          { showForm ? 'Get Story' : 'Restart'}
+      </button>
+    </form>
+
     </div>
   );
 }
